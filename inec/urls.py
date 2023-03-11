78a8ded4461f8f660958aed9e0210528a6572896
Detroit98 <duruakuebuka@gmail.com>
2023-03-11 18:49:16 +0100

    select lga fixes

--- inec/urls.py
+++ inec/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('select_lga/', views.select_lga, name='select_lga'),
     path('lga_polling_unit_results/', views.lga_polling_unit_results, name='lga_polling_unit_results'),
     path('new_polling_unit_result/', views.new_polling_unit_result, name='new_polling_unit_result'),
+    path('', views.select_lga, name='select_lga'),
 ]
